d5b833834a030e79ccc6c59dff425489ea35cbdf
Willy Aguirre <marti1125@gmail.com>
2021-03-21 02:14:49 -0300

    upload images

--- books/settings.py
+++ books/settings.py
@@ -168,3 +168,6 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
